555e5a03bc9e6f6f7d5fd4592405e4cbd850a99c
Kunal Tripathi <159307824+kunal-tri@users.noreply.github.com>
2026-05-28 14:23:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import streamlit as st
 import pandas as pd
 
 from io import StringIO
-
+from merge_csv import merge_csv_with_s3
 from incre import run_incremental_scd2_pipeline
 
 # PAGE CONFIG
@@ -169,215 +169,22 @@ if input_mode == "Upload CSV":
                             "nbfc_document_data.csv"
                         )
 
-                    uploaded_file.seek(0)
-
-                    new_df = pd.read_csv(
-                        uploaded_file,
-                        dtype=str
+                    merge_result = merge_csv_with_s3(
+                        s3_client=s3_client,
+                        bucket_name=BUCKET_NAME,
+                        s3_key=s3_key,
+                        uploaded_file=uploaded_file,
+                        date_cols=date_cols
                     )
-
-                    # CLEAN NEW DATA
-
-                    new_df.columns = (new_df.columns.str.strip())
-
-                    for col in new_df.columns:
-
-                        new_df[col] = (
-                            new_df[col]
-                            .fillna("")
-                            .astype(str)
-                            .str.strip()
-                        )
-
-                    # DATE PARSING
-
-                    date_cols = DATE_COLUMNS[selected_table]
-
-                    for col in date_cols:
-
-                        if col in new_df.columns:
-
-                            new_df[col] = pd.to_datetime(
-                                new_df[col],
-                                errors="coerce"
-                            )
-
-                    # LOAD EXISTING CSV FROM S3
-                    try:
-
-                        response = (
-                            s3_client.get_object(
-                                Bucket=BUCKET_NAME,
-                                Key=s3_key
-                            )
-                        )
-
-                        existing_csv = (
-                            response["Body"]
-                            .read()
-                            .decode("utf-8")
-                        )
-
-                        existing_df = pd.read_csv(
-                            StringIO(existing_csv),
-                            dtype=str
-                        )
-
-                        existing_df.columns = (
-                            existing_df.columns
-                            .str.strip()
-                        )
-
-                        for col in existing_df.columns:
-
-                            existing_df[col] = (
-                                existing_df[col]
-                                .fillna("")
-                                .astype(str)
-                                .str.strip()
-                            )
-
-                        # EXISTING DATE PARSING
-
-                        for col in date_cols:
-
-                            if col in existing_df.columns:
-
-                                existing_df[col] = (
-                                    pd.to_datetime(
-                                        existing_df[col],
-                                        errors="coerce"
-                                    )
-                                )
-
-                        st.info(
-                            f"Existing Rows "
-                            f"in S3: "
-                            f"{len(existing_df)}"
-                        )
-
-                    except Exception:
-
-                        existing_df = (pd.DataFrame())
-
-                        st.warning(
-                            "No existing CSV "
-                            "found in S3"
-                        )
-
-                    # REMOVE SCD2 COLUMNS
-                    scd_columns = [
-
-                        "start_date",
-                        "end_date",
-                        "is_current",
-                        "etl_timestamp"
-                    ]
-
-                    existing_df = existing_df.drop(
-                        columns=[
-                            col for col in scd_columns
-                            if col in existing_df.columns
-                        ],
-                        errors="ignore"
-                    )
-
-                    new_df = new_df.drop(
-                        columns=[
-                            col for col in scd_columns
-                            if col in new_df.columns
-                        ],
-                        errors="ignore"
-                    )
-
-                    # ALIGN COLUMNS
-
-                    if not existing_df.empty:
-
-                        all_columns = list(set(existing_df.columns) | set(new_df.columns))
-
-                        existing_df = (
-                            existing_df.reindex(
-                                columns=all_columns
-                            )
-                        )
-
-                        new_df = (
-                            new_df.reindex(
-                                columns=all_columns
-                            )
-                        )
-
-                    # HASH COMPARISON
-                    if existing_df.empty:
-                        incremental_df = (new_df.copy())
-                    else:
-                        existing_row_hash = (
-                            existing_df
-                            .fillna("")
-                            .astype(str)
-                            .apply(lambda row:"|".join(map(str,row.values)),axis=1))
-
-                        new_row_hash = (
-                            new_df
-                            .fillna("")
-                            .astype(str)
-                            .apply(lambda row:"|".join(map(str,row.values)),axis=1))
-
-                        incremental_df = new_df[~new_row_hash.isin(set(existing_row_hash))]
-
-                    # HANDLE NO NEW ROWS
-                    if (pipeline_mode=="Incremental SCD2 Pipeline"):
-
-                        if incremental_df.empty:
-
-                            st.warning(
-                                "No new incremental "
-                                "rows found."
-                            )
-
-                            st.stop()
-
-                    # MERGE CSV
-                    combined_df = pd.concat(
-                        [
-                            existing_df,
-                            incremental_df
-                        ],
-                        ignore_index=True
-                    )
-
-                    before_dedup = (len(combined_df))
-
                     
-
-                    after_dedup = (len(combined_df))
-
-                    duplicates_removed = (before_dedup - after_dedup)
-
-                    # SAVE MERGED CSV TO S3
-
-                    csv_buffer = StringIO()
-
-                    save_df = combined_df.copy()
-
-                    for col in date_cols:
-
-                        if col in save_df.columns:
-
-                            save_df[col] = (save_df[col].astype(str))
-
-                    save_df.to_csv(
-                        csv_buffer,
-                        index=False
-                    )
-
-                    s3_client.put_object(
-                        Bucket=BUCKET_NAME,
-                        Key=s3_key,
-                        Body=csv_buffer.getvalue()
-                    )
-
+                    existing_df = merge_result["existing_df"]
+                    
+                    new_df = merge_result["new_df"]
+                    
+                    incremental_df = merge_result["incremental_df"]
+                    
+                    combined_df = merge_result["combined_df"]
+                    
                     st.success(
                         "CSV merged "
                         "successfully "
